4698c6959fc39a902e27a3ee0491b3e4c2b26cd5
Natsuki Ito <sf.fm.forza@outlook.com>
2015-09-13 15:56:17 +0900

    位置取得まで作った

---
+++ wcg-client/JsonResults.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcg_client
+{
+    public class spotApiResult
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+    }
+}
--- wcg-client/MainPage.xaml.cs
+++ wcg-client/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,6 +15,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=391641 を参照してください
 
@@ -23,6 +27,8 @@ namespace wcg_client
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Geolocator _geolocator;
+
         private Spot ownPlace;
         private List<Spot> spots;
 
@@ -40,6 +46,7 @@ namespace wcg_client
         /// このプロパティは、通常、ページを構成するために使用します。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            // MapControlにBingマップを重ねる
             mainMap.Style = MapStyle.None; // オリジナルのタイルを消す
 
             var mapTile = new MapTileSource
@@ -49,17 +56,41 @@ namespace wcg_client
             };
             mainMap.TileSources.Insert(0, mapTile);
 
-            // 自己位置を取得
+            getSpots();
+        }
 
+        private async void getSpots()
+        {
+            Debug.WriteLine("Start getSpot()");
+            // 自己位置を取得
+            // var accessStatus = await Geolocator.RequestAccessAsync(); // Win10
+            if (_geolocator == null)
+            {
+                _geolocator = new Geolocator();
+            }
+            _geolocator.ReportInterval = 60000;
+            _geolocator.DesiredAccuracyInMeters = (uint)Math.Round((double)App.Current.Resources["defaultAccuracy"]);
+            Debug.WriteLine("acc: " + _geolocator.DesiredAccuracyInMeters.ToString());
+
+            var pos = await _geolocator.GetGeopositionAsync();
+            var lat = pos.Coordinate.Point.Position.Latitude;
+            var lon = pos.Coordinate.Point.Position.Longitude;
+            _geolocator = null;
+
+            Debug.WriteLine("coord: " + lat.ToString() + "," + lon.ToString());
+            ownPlace = new Spot(mainMap, "", lat, lon);
 
             // HTTP リクエスト (Spotリスト取得)
-
+            //var httpClient = new HttpClient();
+            //var response = await httpClient.GetAsync((string)App.Current.Resources["WcgServiceUrl"]);
+            //Debug.WriteLine(response.ToString());
+            //var result = JsonConvert.DeserializeObject<spotApiResult>(json);
 
             // リスト展開 (spotsに格納)
 
 
             // TODO
-
         }
+
     }
 }
